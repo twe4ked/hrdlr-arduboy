59eb07ded0ae7d83d81ee381b7c2809b86d1ca62
Odin Dutton <odindutton@gmail.com>
2016-12-18 19:04:53 +1100

    Actually use a .cpp file

--- src/hrdlr.ino
+++ src/hrdlr.ino
@@ -1,31 +1,9 @@
-#include <Math.h>
-#include <bitmaps/banner.h>
-#include <bitmaps/coin.h>
-#include <bitmaps/hurdle.h>
-#include <bitmaps/player.h>
-#include <bitmaps/speaker.h>
-
-#include <globals.h>
 #include <game.h>
 
-boolean nextFrame() {
-  if (arduboy.nextFrame()) {
-    buttons.poll();
-    return true;
-  }
-  return false;
-}
-
 void setup() {
-  ensureValidHighScore();
-  reset();
-  arduboy.beginNoLogo();
+  gameSetup();
 }
 
 void loop() {
-  if (!(nextFrame())) return;
-
-  arduboy.clear();
-  run();
-  arduboy.display();
+  gameLoop();
 }
